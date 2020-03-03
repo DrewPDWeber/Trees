b9e7953c793dfca679c91d3dd93fb9177b2f8e29
ImSpare <52802045+ImSpare@users.noreply.github.com>
2020-03-02 20:31:22 -0800

    Update GeoTypeInfo.cs

--- Trees_Mapbox_Rui/Trees/Assets/Structures/GeoTypeInfo.cs
+++ Trees_Mapbox_Rui/Trees/Assets/Structures/GeoTypeInfo.cs
@@ -1,8 +1,19 @@
-﻿using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine;
 
 public class GeoTypeInfo
 {
+	public GeoTypeInfo(int type,GeoPosition pos)
+	{
+		Type = type;
+		GeoLocation = pos;
+	}
+
+	[JsonProperty("type")] 
 	public int Type { get; set; }
+
+	[JsonProperty("geolocation")] 
 	public GeoPosition GeoLocation { get; set; }
 }
+
 
