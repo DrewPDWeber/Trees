ed93a68db24f82289b35dc872ffd7156181fb47c
ImSpare <52802045+ImSpare@users.noreply.github.com>
2020-03-02 20:30:51 -0800

    Update GeoPosition.cs

--- Trees_Mapbox_Rui/Trees/Assets/Structures/GeoPosition.cs
+++ Trees_Mapbox_Rui/Trees/Assets/Structures/GeoPosition.cs
@@ -1,11 +1,18 @@
-﻿public class GeoPosition
+using Newtonsoft.Json;
+
+public class GeoPosition
 {
 		public GeoPosition(double latitude, double longitude)
 		{
 			Latitude = latitude;
 			Longitude = longitude;
 		}
+
+		[JsonProperty("altitude")]
 		public double Altitude { get; set; }
+		
+		[JsonProperty("latitude")]
 		public double Latitude { get; set; }
+		[JsonProperty("longitude")]
 		public double Longitude { get; set; }
 }
