532e4df9388c5686aa9ed9bc0fafb98bf38bfe82
mike-tronic <56046249+mike-tronic@users.noreply.github.com>
2020-03-05 14:20:14 -0800

    added menu
    can swith between menue and map onclick

---
+++ Trees_Mapbox_Rui/Trees/Assets/Scenes/World/toMainMenu1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class toMainMenu1 : MonoBehaviour
+{
+    public void toMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
