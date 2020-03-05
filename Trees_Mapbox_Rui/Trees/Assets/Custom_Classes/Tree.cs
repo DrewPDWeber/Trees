52e4f4c770d67826e788b9d1bc7759f6430460f1
ImSpare <52802045+ImSpare@users.noreply.github.com>
2020-03-05 15:02:25 -0800

    update,mvcfm,sad

--- Trees_Mapbox_Rui/Trees/Assets/Custom_Classes/Tree.cs
+++ Trees_Mapbox_Rui/Trees/Assets/Custom_Classes/Tree.cs
@@ -17,11 +17,46 @@ public class Tree : MonoBehaviour
     [JsonProperty("geotype")]
     public GeoTypeInfo GeoType;
 
+
+    static bool _display = false, hiding;
+    static string content;
+
+    static Rect windowRect = new Rect(20, 20, 200, 200);
+    const float time = 5f; //Seconds to read the text
+
+    IEnumerator Hide()
+    {
+        if (!hiding)
+        {
+            hiding = true;
+            yield return new WaitForSeconds(time);
+            hiding = false;
+            _display = false;
+        }
+        yield return null;
+    }
+
+    void OnGUI()
+    {
+        if (!_display)
+            return;
+
+        windowRect = GUI.Window(0, windowRect, DoMyWindow, content);
+
+        StartCoroutine("Hide");
+    }
+
+    void DoMyWindow(int windowID)
+    {
+        GUI.DragWindow(new Rect(0, 0, 10000, 20));
+    }
     private void OnMouseDown()
     {
-        Debug.Log("ID:"+ Id);
-        Debug.Log("Name:"+ Name);
-        Debug.Log("Species:"+ Species);
-        Debug.Log("Geotype:"+ GeoType);
+        Debug.Log("ID:" + Id);
+        Debug.Log("Name:" + Name);
+        Debug.Log("Species:" + Species);
+        Debug.Log("Geotype:" + GeoType);
+        content = "ID:" + Id + "\n" + "Name:" + Name + "\n" + "Species:" + Species + "\n";
+        _display = true;
     }
 }
--- Trees_Mapbox_Rui/Trees/Assets/Custom_Classes/TreeFactory.cs
+++ Trees_Mapbox_Rui/Trees/Assets/Custom_Classes/TreeFactory.cs
@@ -7,7 +7,7 @@ public class TreeFactory : Singleton<TreeFactory>
 {
     [SerializeField] private Tree[] closestTrees;
     [SerializeField] private Player player;
-    
+
     const int MIN_RANGE = 10;
     const int MAX_RANGE = 180;
 
@@ -20,7 +20,16 @@ public class TreeFactory : Singleton<TreeFactory>
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < closestTrees.Length; i++)
+        /*
+        Input.location.Start();
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        */
+        for (int i = 0; i < closestTrees.Length; i++)
         {
             InstantiateTree(i);
         }
@@ -31,9 +40,12 @@ public class TreeFactory : Singleton<TreeFactory>
         float x = player.transform.position.x + GenerateRange();
         float z = player.transform.position.z + GenerateRange();
         float y = player.transform.position.y;
-
-        
-        string treeName = TreeTypes.TreeSpecies[Random.Range(0,TreeTypes.TreeSpecies.Count)];
+        /*
+        Debug.Log(Input.location.lastData.latitude);
+        Debug.Log(y);
+        Debug.Log(z);
+        */
+        string treeName = TreeTypes.TreeSpecies[Random.Range(0, TreeTypes.TreeSpecies.Count)];
         string treeSpecies = TreeTypes.TreeNames[Random.Range(0, TreeTypes.TreeNames.Count)];
         GeoTypeInfo treeGeo = new GeoTypeInfo(1, new GeoPosition(x, z));
 
@@ -51,6 +63,4 @@ public class TreeFactory : Singleton<TreeFactory>
     {
         return Random.Range(MIN_RANGE, MAX_RANGE);
     }
-
-    
 }
