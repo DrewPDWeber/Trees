0782558406c33f9ee232068d990146781bb7790e
mike-tronic <56046249+mike-tronic@users.noreply.github.com>
2020-04-20 15:35:45 -0700

    half fade
    half fade

---
+++ unity_app/Trees/Assets/Animation/LevelChanger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    
+    public void FadeToScene()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+}
--- unity_app/Trees/Assets/Scenes/World/toMainMenu1.cs
+++ unity_app/Trees/Assets/Scenes/World/toMainMenu1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class toMainMenu1 : MonoBehaviour
 {
+
     public void toMenu()
     {
         SceneManager.LoadScene("MainMenu");
